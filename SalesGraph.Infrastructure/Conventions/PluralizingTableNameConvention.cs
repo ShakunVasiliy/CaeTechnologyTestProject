e79994911430d767b341f55709b37b1a7885cce3
vshakun <vshakun@lpglo.com>
2025-06-11 17:14:39 +0300

    add pluralizing table name convention.

---
+++ SalesGraph.Infrastructure/Conventions/PluralizingTableNameConvention.cs
@@ -0,0 +1,21 @@
+﻿using Common.Helpers;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+
+namespace SalesGraph.Infrastructure.Conventions
+{
+    internal class PluralizingTableNameConvention : IModelFinalizingConvention
+    {
+        public void ProcessModelFinalizing(IConventionModelBuilder modelBuilder, IConventionContext<IConventionModelBuilder> context)
+        {
+            foreach (var entityType in modelBuilder.Metadata.GetEntityTypes())
+            {
+                if (entityType.GetTableName() is null)
+                {
+                    entityType.SetTableName(WordHelper.Pluralize(entityType.DisplayName()));
+                }
+            }
+        }
+    }
+}
