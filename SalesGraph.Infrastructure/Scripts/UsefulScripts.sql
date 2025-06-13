bd679cfa3348a0d3c26016b05877d977a2b6073d
vshakun <vshakun@lpglo.com>
2025-06-13 15:14:05 +0300

    add useful scripts.

---
+++ SalesGraph.Infrastructure/Scripts/UsefulScripts.sql
@@ -0,0 +1,31 @@
+﻿select *
+from [dbo].[DaySales];
+
+select *
+from [dbo].[WeekSales];
+
+select *
+from [dbo].[MonthSales];
+
+select *
+from [dbo].[QuarterSales];
+
+
+/*
+
+DELETE FROM [dbo].[Sales];
+DBCC CHECKIDENT ('[dbo].[Sales]', RESEED, 0);
+
+DELETE FROM [dbo].[DaySales];
+DBCC CHECKIDENT ('[dbo].[DaySales]', RESEED, 0);
+
+DELETE FROM [dbo].[WeekSales];
+DBCC CHECKIDENT ('[dbo].[WeekSales]', RESEED, 0);
+
+DELETE FROM [dbo].[MonthSales];
+DBCC CHECKIDENT ('[dbo].[MonthSales]', RESEED, 0);
+
+DELETE FROM [dbo].[QuarterSales];
+DBCC CHECKIDENT ('[dbo].[QuarterSales]', RESEED, 0);
+
+*/
